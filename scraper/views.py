dac48a854ea69eded638b0abb221e39d6afd926d
Kiki <zoeluoluo05673@gmail.com>
2024-03-24 22:46:23 +0800

    feat: scrape the data and return

--- scraper/views.py
+++ scraper/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from bs4 import BeautifulSoup
 import requests
 
 # Create your views here.
@@ -9,6 +10,12 @@ def index(request):
         # get the HTML of target site
         response = requests.get(url)
         if response.status_code == 200:
-            return HttpResponse(HttpResponse("get data success!"))
+            html = BeautifulSoup(response.text, 'html.parser')
+            # get p element in the html
+            contents = html.find_all('p')
+            # convert p into text
+            texts = [p.get_text() for p in contents]
+            renderContent = '<br>'.join(texts)
+            return HttpResponse(f"Scraped text: {renderContent}")
         return HttpResponse(HttpResponse(f"URL submitted: {url} but fetch data error"))
     return render(request, 'form.html')
