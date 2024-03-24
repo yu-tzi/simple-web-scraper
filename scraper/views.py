549517de05784b62dd674e0952e2041b29361cf4
Kiki <zoeluoluo05673@gmail.com>
2024-03-24 14:31:31 +0800

    feat: add url config and index function

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(req):
+    return HttpResponse('Hello world !')
+# render the form
+# process the URL and scrape the webpage
