49a24367b94e0400a9af6b391eadaac9ea798bca
Kiki <zoeluoluo05673@gmail.com>
2024-03-24 22:33:51 +0800

    feat: fetch target url and show status

--- scraper/views.py
+++ scraper/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
         url = request.POST.get('url')
-        return HttpResponse(HttpResponse(f"URL submitted: {url}"))
+        # get the HTML of target site
+        response = requests.get(url)
+        if response.status_code == 200:
+            return HttpResponse(HttpResponse("get data success!"))
+        return HttpResponse(HttpResponse(f"URL submitted: {url} but fetch data error"))
     return render(request, 'form.html')
