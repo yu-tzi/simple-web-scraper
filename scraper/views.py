498676d378711679a21253140b7fe8694063501f
Kiki <zoeluoluo05673@gmail.com>
2024-03-24 23:04:31 +0800

    feat: add try catch

--- scraper/views.py
+++ scraper/views.py
@@ -7,15 +7,18 @@ import requests
 def index(request):
     if request.method == "POST":
         url = request.POST.get('url')
-        # get the HTML of target site
-        response = requests.get(url)
-        if response.status_code == 200:
-            html = BeautifulSoup(response.text, 'html.parser')
-            # get p element in the html
-            contents = html.find_all('p')
-            # convert p into text
-            texts = [p.get_text() for p in contents]
-            renderContent = '\n'.join(texts)
-            return render(request, 'index.html', { 'renderContent': renderContent})
-        return HttpResponse(HttpResponse(f"URL submitted: {url} but fetch data error"))
+        try:
+            # get the HTML of target site
+            response = requests.get(url)
+            if response.status_code == 200:
+                html = BeautifulSoup(response.text, 'html.parser')
+                # get p element in the html
+                contents = html.find_all('p')
+                # convert p into text
+                texts = [p.get_text() for p in contents]
+                renderContent = '\n'.join(texts)
+                return render(request, 'index.html', { 'renderContent': renderContent})
+        except:
+            # when sth go wrong
+            return render(request, 'index.html', { 'renderContent': 'somthing went wrong :( \nplz try another url'})
     return render(request, 'index.html')
